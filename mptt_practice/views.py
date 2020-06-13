0e26cc76fb9b01640bd7c6153586cef4811319e1
Janell.Huyck <Janell.Huyck@outlook.com>
2020-06-12 22:56:43 -0400

    displays information on home page.

--- mptt_practice/views.py
+++ mptt_practice/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from mptt_practice.models import Mptt_file
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'nodes': Mptt_file.objects.all()})
